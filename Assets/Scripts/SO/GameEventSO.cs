af572f10432b06e0efbe8e7af47761320c61c8a8
Greenshadow369 <lechinguyen369@gmail.com>
2024-03-23 23:41:30 -0700

    Update order visual
    +update order sprite and structure
    +add order selected visual(no function)
    +prepare game event listener and scriptable object

---
+++ Assets/Scripts/SO/GameEventSO.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Game Event")]
+public class GameEvent : ScriptableObject
+{
+    private readonly List<GameEventListener> eventListenerList = new List<GameEventListener>();
+
+    public void Raise()
+    {
+        for(int i = eventListenerList.Count - 1; i >= 0; i--)
+        {
+            eventListenerList[i].OnEventRaised();
+        }
+    }
+
+    public void RegisterListener(GameEventListener listener)
+    {
+        if(!eventListenerList.Contains(listener))
+        {
+            eventListenerList.Add(listener);
+        }
+    }
+
+    public void UnregisterListener(GameEventListener listener)
+    {
+        if(eventListenerList.Contains(listener))
+        {
+            eventListenerList.Remove(listener);
+        }
+    }
+}
