9de0cdc1ca2cd37b84e4cc2467959bc53c4bd81e
Greenshadow369 <lechinguyen369@gmail.com>
2025-08-26 17:47:04 -0700

    Add Menu Panel Transition
    +game menu now have transition, not yet  apply to mode menu
    +add canvas group to those panel

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class MenuManager : MonoBehaviour
 {
@@ -15,26 +16,101 @@ public class MenuManager : MonoBehaviour
 
     [SerializeField] GameObject gameMenu;
     [SerializeField] GameObject modeMenu;
+    private Tween gameMenuTween;
+    private CanvasGroup gameMenuCanvasGroup;
+    private CanvasGroup modeMenuCanvasGroup;
 
-    private void ChangeActiveState(GameObject gameObject)
+    private void Awake()
     {
-        if(gameObject.activeSelf)
+        modeMenuCanvasGroup = modeMenu.GetComponent<CanvasGroup>();
+        if (modeMenuCanvasGroup == null)
         {
-            gameObject.SetActive(false);
+            // safety: auto add one if forgot to add one in the inspector
+            modeMenuCanvasGroup = modeMenu.AddComponent<CanvasGroup>();
+        }
+
+        gameMenuCanvasGroup = gameMenu.GetComponent<CanvasGroup>();
+        if (gameMenuCanvasGroup == null)
+        {
+            // safety: auto add one if forgot to add one in the inspector
+            gameMenuCanvasGroup = gameMenu.AddComponent<CanvasGroup>();
+        }
+
+        //Set up animation for reuse
+        gameMenuTween = gameMenu.GetComponent<RectTransform>().DOAnchorPosX(2000, 1)
+                                 .From()
+                                 .SetEase(Ease.OutQuad)
+                                 .SetAutoKill(false)
+                                 .Pause();
+        gameMenuTween.Restart();
+
+        gameMenuTween.OnPlay(() =>
+            {
+                gameMenuCanvasGroup.blocksRaycasts = false;
+            }
+        );
+
+        gameMenuTween.OnRewind(() =>
+            {
+                gameMenu.SetActive(false);
+                gameMenuCanvasGroup.blocksRaycasts = true;
+            }
+        );
+            
+        gameMenuTween.OnComplete(()=>
+            {
+                gameMenuCanvasGroup.blocksRaycasts = true;
+            }
+        );
+    }
+
+    private void ChangeGameMenuActiveState(GameObject target)
+    {
+        if (target.activeSelf)
+        {
+            //Animate game menu transition out
+            //target.GetComponent<RectTransform>().DOAnchorPosX(-2000, 1).From().SetEase(Ease.OutQuad).OnComplete(()
+            //    => target.SetActive(false));
+
+            gameMenuTween.PlayBackwards();
+
         }
         else
         {
-            gameObject.SetActive(true);
+
+            target.SetActive(true);
+            gameMenuTween.Restart();
+            
+            //Animate game menu transition in
+            //target.GetComponent<RectTransform>().DOAnchorPosX(2000, 1).From().SetEase(Ease.OutQuad);
+        }
+    }
+
+    private void ChangeModeMenuActiveState(GameObject target)
+    {
+        if (target.activeSelf)
+        {
+            //Animate game menu transition out
+            //target.GetComponent<RectTransform>().DOAnchorPosX(-2000, 1).From().SetEase(Ease.OutQuad).OnComplete(()
+            //    => target.SetActive(false));
+            target.SetActive(false);
+
+        }
+        else
+        {
+            target.SetActive(true);
+            //Animate mode menu transition in
+            //target.GetComponent<RectTransform>().DOAnchorPosX(-2000, 1).From().SetEase(Ease.OutQuad);
         }
     }
 
     public void ChangeGameMenuActiveState()
     {
-        ChangeActiveState(gameMenu);
+        ChangeGameMenuActiveState(gameMenu);
     }
 
     public void ChangeModeMenuActiveState()
     {
-        ChangeActiveState(modeMenu);
+        ChangeModeMenuActiveState(modeMenu);
     }
 }
